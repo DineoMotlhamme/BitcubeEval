f0f72650529c460031ddfa5476d390a7a8db6a5e
Motlhamme Dineo <216004616@stud.cut.ac.za>
2020-03-11 19:21:33 +0200

    Add project files.

---
+++ BitcubeEval/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BitcubeEval.Startup))]
+namespace BitcubeEval
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
